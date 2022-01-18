6c2d7cab1e5d4faeb76f9b3b759d92d1c92198bb
gkuer <gkuer95@gmail.com>
2022-01-18 15:10:13 +0900

    FEAT: new project [#S06P12B206-21]

---
+++ back/together-homt/src/test/java/com/ssafy/togetherhomt/TogetherHomtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.togetherhomt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherHomtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
