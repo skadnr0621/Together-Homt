0b36d3fa7f992a03e01c6d21f9a2581185345e41
이호열 <xsrsx2000@gmail.com>
2022-01-27 17:46:50 +0900

    FEAT: 알림 기능

--- back/together-homt/src/main/java/com/ssafy/togetherhomt/notification/NotificationService.java
+++ back/together-homt/src/main/java/com/ssafy/togetherhomt/notification/NotificationService.java
@@ -2,11 +2,13 @@ package com.ssafy.togetherhomt.notification;
 
 import com.ssafy.togetherhomt.common.CommonService;
 import com.ssafy.togetherhomt.user.User;
+import com.ssafy.togetherhomt.user.UserRepository;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -14,14 +16,48 @@ public class NotificationService {
 
     private CommonService commonService;
 
+    private NotificationRepository notificationRepository;
+    private UserRepository userRepository;
 
-    public void sendNotification(User receiver) {
+
+    /*** 알림 조회 ***/
+    public List<NotificationDto> getNotification(User receiver) {
+        List<NotificationDto> notificationList = new ArrayList<>();
+        for (Notification notification : notificationRepository.findByReceiver(receiver))
+            notificationList.add(new NotificationDto(notification));
+        return notificationList;
+    }
+
+    /*** 알림 전송 ***/
+    public Notification sendNotification(NotificationDto notificationDto) {
+        User receiver = userRepository.findByEmail(notificationDto.getReceiver().getEmail());
+        if (receiver == null)
+            return null;
+
         User sender = commonService.getLoginUser();
 
-//        Notification notification = new Notification();
-//        notification.setSender(sender);
-//        notification.setReceiver(receiver);
-//        notification.setNotificationType(type);
+        Notification notification = Notification.builder()
+                .sender(sender)
+                .receiver(receiver)
+                .notificationType(notificationDto.getNotificationType())
+                .build();
+        return notificationRepository.save(notification);
+    }
+
+    /*** 알림 확인(삭제) ***/
+    public Notification readNotification(Long notificationId) {
+        Optional<Notification> optNotification = notificationRepository.findById(notificationId);
+        if (!optNotification.isPresent())
+            return null;
+
+        Notification notification = optNotification.get();
+
+        User me = commonService.getLoginUser();
+        if (!me.getEmail().equals(notification.getReceiver().getEmail()))
+            return null;
+
+        notificationRepository.delete(notification);
+        return notification;
     }
 
 }
