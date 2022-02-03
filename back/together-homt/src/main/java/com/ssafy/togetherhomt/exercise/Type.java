c307633ff2f4efc5483b4a6d67071d99eb848339
이호열 <xsrsx2000@gmail.com>
2022-02-03 18:49:42 +0900

    Delete back/ for merge

--- back/together-homt/src/main/java/com/ssafy/togetherhomt/exercise/Type.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.togetherhomt.exercise;
-
-public enum Type {
-    Stretching,
-    WeightTraining
-}
--- back/together-homt/src/main/java/com/ssafy/togetherhomt/user/group/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.togetherhomt.user.group;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<Group, Long> {
-    Group findByName(String name);
-}
