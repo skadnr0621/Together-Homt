145d52b4623f91c3f8f6ca8ea6ed118f72df6635
ghkdlxld <ohsg97@gmail.com>
2022-02-14 22:28:44 +0900

    DESIGN: [FE] CSS 디자인

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,19 @@
 <template>
+<div>
+  <div class="bg">
+    <div class="circle-blue right-1"></div>
+    <div class="circle-big"></div>
+    <div class="circle-blue left-2"></div>
+    <div class="circle-yellow left-1"></div>
+    <div class="circle-yellow right-4"></div>
+    <div class="circle-green left-3"></div>
+    <div class="circle-green right-2"></div>
+    <div class="circle-pink right-3"></div>
+  </div>
   <div id="app">
-    <div class="bg">
-      <div class="circle-blue right-1"></div>
-      <div class="circle-big"></div>
-      <div class="circle-blue left-2"></div>
-      <div class="circle-yellow left-1"></div>
-      <div class="circle-yellow right-4"></div>
-      <div class="circle-green left-3"></div>
-      <div class="circle-green right-2"></div>
-      <div class="circle-pink right-3"></div>
-    </div>
     <router-view />
   </div>
+</div>
 </template>
 
 <script>
