eedf71fc4c9c8f3a9fab063fcb36ae4d75098711
이호열 <xsrsx2000@gmail.com>
2022-02-15 01:11:49 +0900

    REFACTOR: moved 'Account' related functions, deleted DTOs not in use anymore FEAT: add username(닉네임) modification

--- back/together-homt/src/main/java/com/ssafy/togetherhomt/user/UserController.java
+++ back/together-homt/src/main/java/com/ssafy/togetherhomt/user/UserController.java
@@ -22,39 +22,11 @@ import java.util.List;
 @AllArgsConstructor
 public class UserController {
 
+    /*** Service ***/
     private final UserService userService;
-    private final CommonService commonService;
-    private final MailingService mailConfirmService;
-
+    /*** Repository ***/
     private final UserRepository userRepository;
 
-    @ApiOperation(value = "회원 가입", notes = "회원 가입")
-    @ApiResponses({
-            @ApiResponse(code = 500, message = "Internal Server Error"),
-            @ApiResponse(code = 409, message = "이미 존재하는 유저입니다. 회원가입에 실패하였습니다."),
-            @ApiResponse(code = 200, message = "200 + \"success\". 회원가입에 성공하였습니다.")
-    })
-    @PostMapping("/users")
-    public ResponseEntity<?> signup(@Valid @RequestBody SignupDto signupDto) {
-        String result = userService.signup(signupDto);
-
-        if ("success".equals(result))
-            return new ResponseEntity<>("회원가입에 성공하였습니다.", HttpStatus.OK);
-        else if ("duplicate".equals(result))
-            return new ResponseEntity<>("이미 가입된 계정 주소입니다.", HttpStatus.CONFLICT);
-        else
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @ApiOperation(value = "회원 가입 승인 이메일", notes = "승인 이메일 발송")
-    @ApiResponses({
-            @ApiResponse(code = 500, message = "존재하지 않는 계정입니다."),
-            @ApiResponse(code = 200, message = "회원가입 인증 코드가 정상적으로 발송되었습니다.")
-    })
-    @PostMapping("/auth/signup/confirm")
-    public ResponseEntity<String> confirmMail(@RequestBody String email) throws Exception {
-        return ResponseEntity.ok(mailConfirmService.sendSimpleMessage(email));
-    }
 
     @ApiOperation(value = "전체 회원 조회", notes = "전체 회원 정보 조회")
     @GetMapping("/users")
@@ -74,37 +46,6 @@ public class UserController {
             return new ResponseEntity<>("존재하지 않는 유저입니다.", HttpStatus.BAD_REQUEST);
         else
             return ResponseEntity.ok(userService.getProfile(user));
-    }
-
-    @ApiOperation(value = "회원 탈퇴", notes = "회원 탈퇴")
-    @ApiResponses({
-            @ApiResponse(code = 400, message = "잘못된 요청입니다. 계정을 명시하지 않았거나 로그인 정보와 맞지 않습니다."),
-            @ApiResponse(code = 200, message = "회원 탈퇴에 성공하였습니다.")
-    })
-    @DeleteMapping("/users/{email}")
-    public ResponseEntity<?> withdraw(){
-        userService.withdraw();
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @ApiOperation(value = "비밀번호 변경", notes = "비밀번호 찾기 후 생성된 난수로 비밀번호 초기화")
-    @ApiResponses({
-            @ApiResponse(code = 400, message = "잘못된 요청입니다. 계정을 명시하지 않았거나 로그인 정보와 맞지 않습니다."),
-            @ApiResponse(code = 200, message = "비밀번호 재설정에 성공하였습니다.")
-    })
-    @PutMapping("/auth/password")
-    public ResponseEntity<?> updatePassword(@RequestBody String newPassword){
-        userService.updatePassword(newPassword);
-        return ResponseEntity.ok("passwordUpdate success");
-    }
-
-    @ApiOperation(value = "비밀번호 초기화", notes = "비밀번호 찾기 과정 중, 난수를 생성하여 비밀번호를 초기화하고, 메일로 전송합니다.")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "임시 재설정 비밀번호를 메일로 전송하는 데 성공하였습니다.")
-    })
-    @PostMapping("/auth/password/reset")
-    public String passwordFind(@RequestBody String email) throws Exception{
-        return userService.passwordFind(email);
     }
 
     @ApiOperation(value = "회원 정보 수정", notes = "회원 정보 수정")
--- back/together-homt/src/main/java/com/ssafy/togetherhomt/user/UserService.java
+++ back/together-homt/src/main/java/com/ssafy/togetherhomt/user/UserService.java
@@ -3,10 +3,7 @@ package com.ssafy.togetherhomt.user;
 import com.ssafy.togetherhomt.common.CommonService;
 import com.ssafy.togetherhomt.config.media.MediaService;
 import com.ssafy.togetherhomt.user.follow.FollowRepository;
-import com.ssafy.togetherhomt.user.info.SignupDto;
-import com.ssafy.togetherhomt.util.Mailing.MailingService;
 import lombok.AllArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,32 +15,15 @@ import java.util.List;
 @AllArgsConstructor
 public class UserService {
 
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
+    /*** Service ***/
     private final CommonService commonService;
-    private final MailingService mailingService;
     private final MediaService mediaService;
-
+    /*** Repository ***/
     private final UserRepository userRepository;
     private final FollowRepository followRepository;
 
 
-    @Transactional
-    public String signup(SignupDto signupDto) {
-        if (userRepository.findByEmail(signupDto.getEmail()) != null)
-            return "duplicate";
 
-        User user = User.builder()
-                .email(signupDto.getEmail())
-                .password(bCryptPasswordEncoder.encode(signupDto.getPassword()))
-                .username(signupDto.getUsername())
-                .role("ROLE_USER")
-                .introduce("")
-                .build();
-        userRepository.save(user);
-
-        return "success";
-    }
 
     public List<UserDto> getAllUsers() {
         List<User> users = userRepository.findAll();
@@ -62,11 +42,20 @@ public class UserService {
     @Transactional
     public UserDto update(UserDto userDto, MultipartFile picture) {
         User user = userRepository.findByEmail(commonService.getLoginUser().getEmail());
-        if (!user.getEmail().equals(userDto.getEmail()) || !user.getUsername().equals(userDto.getUsername())) {
+        if (!user.getEmail().equals(userDto.getEmail())) {
             return null;
         }
 
-        // update user
+        // Username(닉네임) 변경
+        if (!user.getUsername().equals(userDto.getUsername())) {
+            if (userRepository.findByUsername(userDto.getUsername()) != null)
+                return null;
+            else
+                user.setUsername(userDto.getUsername());
+        }
+        // 자기소개 변경
+        user.setIntroduce(userDto.getIntroduce());
+        // 프로필 사진 변경
         if (picture != null) {
             String resourcePath = mediaService.save(picture);
             if (resourcePath == null)
@@ -74,75 +63,11 @@ public class UserService {
             else
                 user.setImagePath(resourcePath);
         }
-        user.setIntroduce(userDto.getIntroduce());
 
         userRepository.save(user);
         return this.builder(user, true);
     }
 
-    @Transactional
-    public void withdraw(){
-        User user = userRepository.findByEmail(commonService.getLoginUser().getEmail());
-        userRepository.delete(user);
-    }
-
-    @Transactional
-    public void updatePassword(String newPassword){
-        User user = userRepository.findByEmail(commonService.getLoginUser().getEmail());
-        user.setPassword(bCryptPasswordEncoder.encode(newPassword));
-        userRepository.save(user);
-    }
-
-    @Transactional
-    public String passwordFind(String email) throws Exception{
-        System.out.println("email = " + email);
-        User user = userRepository.findByEmail(email);
-        String newPassword = mailingService.sendSimpleMessage(email);
-
-        System.out.println("newPassword = " + newPassword);
-        System.out.println("user = " + user);
-
-        user.setPassword(bCryptPasswordEncoder.encode(newPassword));
-
-        userRepository.save(user);
-
-        return "success";
-    }
-
-
-    // --------------------------------------------------
-
-//    private List<UserDto> txFollowerListToUserDtoList(List<Follow> followerList) {
-//        List<UserDto> followers = new ArrayList<>();
-//        for (Follow follow : followerList) {
-//            User follower = follow.getFollower();
-//            followers.add(
-//                    UserDto.builder()
-//                            .email(follower.getEmail())
-//                            .username(follower.getUsername())
-//                            .profileUrl(follower.getProfile_url())
-//                            .introduce(follower.getIntroduce())
-//                            .build()
-//            );
-//        }
-//        return followers;
-//    }
-//
-//    private List<UserDto> txFollowingListToUserDtoList(List<Follow> followingList) {
-//        List<UserDto> followings = new ArrayList<>();
-//        for (Follow follow : followingList) {
-//            User following = follow.getFollowing();
-//            followings.add(
-//                    UserDto.builder()
-//                            .email(following.getEmail())
-//                            .username(following.getUsername())
-//                            .profileUrl(following.getProfile_url())
-//                            .introduce(following.getIntroduce())
-//                            .build()
-//            );
-//        }
-//        return followings;
-//    }
 
     // --------------------------------------------------
 
@@ -154,10 +79,12 @@ public class UserService {
                 .imagePath(user.getImagePath())
                 .introduce(user.getIntroduce());
 
+        User loginUser = commonService.getLoginUser();
+        if (followRepository.findByFollowerAndFollowing(loginUser, user) != null || user.getEmail().equals(loginUser.getEmail()))
+            userDtoBuilder.followed(true);
+
         if (verbose) {
             userDtoBuilder
-//                    .followers(txFollowerListToUserDtoList(user.getFollower()))
-//                    .followings(txFollowingListToUserDtoList(user.getFollowing()))
                     .cntFollower(followRepository.countByFollowing(user))
                     .cntFollowing(followRepository.countByFollower(user));
         }
