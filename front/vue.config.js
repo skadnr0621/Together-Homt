a424e6b70501b07ecde09a2968d3eae0ab40b517
김남욱 <skadnr0621@gmail.com>
2022-02-17 10:14:36 +0000

    Update vue.config.js

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-  devServer: {
-    proxy: "http://localhost:8092/",
-  },
-};
 // module.exports = {
 //   devServer: {
-//     proxy: "http://i6b206.p.ssafy.io:8092",
+//     proxy: "http://localhost:8092/",
 //   },
-// };
+//};
+module.exports = {
+  devServer: {
+    proxy: "http://i6b206.p.ssafy.io:8092",
+  },
+};
