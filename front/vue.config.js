e0e7e810a21c56e0fd2628144d553e73fdbd2466
임채은 <collcr@kakao.com>
2022-02-17 14:09:52 +0000

    Update vue.config.js

--- front/vue.config.js
+++ front/vue.config.js
@@ -3,9 +3,20 @@
 //     proxy: "http://localhost:8092/",
 //   },
 //};
+// module.exports = {
+//   devServer: {
+//     //proxy: "http://i6b206.p.ssafy.io:8092",
+//     proxy: "http://3.38.103.222:8092/api",
+//   },
+// };
+
 module.exports = {
-  devServer: {
-    //proxy: "http://i6b206.p.ssafy.io:8092",
-    proxy: "http://3.38.103.222:8092/api",
-  },
-};
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://3.38.103.222:8092' // 개발서버
+            }
+        }
+    }
+}
+
