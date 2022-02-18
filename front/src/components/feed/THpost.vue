b830741dbc1174f02cf9e6bfa34a2ceeaf080e8d
im-cherry <collcr@kakao.com>
2022-02-18 09:31:47 +0900

    기능 수정

--- front/src/components/feed/THpost.vue
+++ front/src/components/feed/THpost.vue
@@ -176,6 +176,12 @@ export default {
 
     // 피드 댓글 이동
     goComment(name, email, id) {
+      // 상세 피드 가져오기
+      await this.$store.dispatch("feedStore/setFeedInfo", {
+        feedId: id,
+        token: sessionStorage.getItem("jwt"),
+      });
+
       this.$router.push({
         name: "ProfileFeedComment",
         params: { userName: name, email: email, feedId: id },
